bac90fb783c4701ccf5c24b81996c70276710aeb
bargogoden <bargogoden@naver.com>
2021-02-03 11:24:09 +0900

    upload_0203

---
+++ js/script_01.js
@@ -0,0 +1,7 @@
+//script_01.js/파일이름
+//자바스크립트 처음 코딩하기
+//ECMA script 5-이게 웹표준, PC와 반응형 할때는 5로 공부.
+console.log('ECMA script 5 자바스크립트 처음 코딩하기 & node_js 실행하기!!!');
+
+//ECMA script 6 -뷰할때는 6버전으로 공부.
+console.log(`ECMA script 6 자바스크립트 처음 코딩하기 & node_js 실행하기!!!`);
